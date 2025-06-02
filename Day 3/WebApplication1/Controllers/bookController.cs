e38832aad0e60d499dcfd17a26435803fae585bb
codernisarg <nisargpadia@gmail.com>
2025-06-02 11:53:30 +0530

    Add files via upload

---
+++ Day 3/WebApplication1/Controllers/bookController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class bookController : Controller
+    {
+        private readonly Bookservices bookservices;
+        
+        public bookController(Bookservices book ) { 
+            this.bookservices = book;
+        }
+
+        
+        [HttpPost]
+        [Route("Add")]
+        public ActionResult Addbook(book book)
+        {
+            //Bookservices = new Bookservices();
+            this.bookservices.Addbook(book);
+            return Ok("Book added ! ");
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public ActionResult getall()
+        {
+            return Ok(this.bookservices.getall());
+        }
+
+        [HttpGet]
+        [Route("getbyid")]
+        public ActionResult getbyid(int id)
+        {
+            var res = this.bookservices.getbyid(id);
+
+            if(res != null) {
+                return Ok(res);
+            }
+            return NotFound("Book not found ! ");
+        }
+
+    }
+}
---
+++ Day 3/bookapi.services/Services/Bookservices.cs
@@ -0,0 +1,29 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class Bookservices
+    {
+        private List<book> _books;
+
+        public Bookservices()
+        {
+            _books = new List<book>();
+        }
+
+        public void Addbook(book book)
+        {
+            _books.Add(book);
+        }
+
+        public List<book> getall()
+        {
+            return _books;
+        }
+
+        public book? getbyid(int id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+    }
+}
